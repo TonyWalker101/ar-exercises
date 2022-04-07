2f8139e26dc39eecf9729c78ea6523288f050a12
Tony <awalker780@gmail.com>
2022-04-06 20:56:00 -0400

    Completes exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,31 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+mens_clothing_stores = Store.where(mens_apparel: true)
+
+puts "Men's clothing stores: "
+puts mens_clothing_stores
+
+mens_clothing_stores.each_with_index do |store, index|
+  puts "Store ##{index + 1}"
+  puts store.name
+  puts "Annual revenue: #{store.annual_revenue}"
+end
+
+store_fetch = Store.where(womens_apparel: true).where(annual_revenue: 0..1000000)
+
+puts "Women's clothing store w/ < 1M revenue"
+puts store_fetch
+
+store_fetch.each_with_index do |store, index|
+  puts "Store ##{index+1}"
+  puts store.name
+  puts "Annual Revenue: #{store.annual_revenue}"
+end
