f89a9e4975bfd059fe7efb05eeec6051e619dad3
Tony <awalker780@gmail.com>
2022-04-06 21:10:18 -0400

    Completes exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -3,8 +3,24 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
+require "bigdecimal"
 
 puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+revenue_sum = Store.sum(:annual_revenue)
+
+puts "Revenue sum for all stores: "
+puts revenue_sum
+
+revenue_avg = Store.average(:annual_revenue)
+
+puts "Average revenue for all stores: "
+puts revenue_avg
+
+high_earning_stores = Store.where(annual_revenue: 1000000..BigDecimal("Infinity")).count
+
+puts "Number of high earning stores: "
+puts high_earning_stores
